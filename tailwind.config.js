5b75d90f4b4448c2ae2503cf0fcce8f8c344723e
yashirpal <yashveersoftwere06@gmail.com>
2025-04-06 10:20:11 +0530

    Skype Variable Fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         "insta-color": "#e12a72",
         "linkedin-color": "#144679",
         "whatsapp-color": "#075e54",
-        "skypee-color": "#00aff0",
+        "skype-color": "#00aff0",
         "socialbg":"#1b74e4",
         "charcoal":"#333",
         "pdtllocationcolor":"#fd7590",
@@ -32,6 +32,12 @@ module.exports = {
       boxShadow:{
         "pdtl":"0px 4px 6px rgba(0, 0, 0, 0.08)",
         '3xl': '0px 6px 6px 0px rgba(238, 238, 238, 0.2)',
+      },
+      backgroundImage: {
+        'body-background-image': "url('./assets/img/page-bg-dark-1.jpg')",
+      },
+      height: {
+        'manavta-logo': '90px',
       },
       lineHeight: {
         '12': '44px',
